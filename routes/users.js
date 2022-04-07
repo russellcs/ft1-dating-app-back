0768ace8b86778368ffcc6da3f9213639daa427f
TF <tf@tfs-imac.lan>
2022-04-07 21:31:36 +0100

    users queries and post connections

--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,36 @@ const fs = require("fs");
 
 // add new user (email only)
 app.post("/", async (req, res) => {
+  console.log(req.body);
   try {
-    await pConnection(queries.addNewUser(req.body.email));
+    //adds to users table
+    const result = await pConnection(
+      queries.addNewUser(req.body.email, req.body.password)
+    );
+    //sets to personalDetails
+    await pConnection(
+      queries.setUserPersonalDetails(req.body, result.insertId)
+    );
+    // sets logged in status
+    await pConnection(queries.setLogIn(result.insertId));
+    //sets users preferences
+    await pConnection(queries.setUserPreferences(req.body, result.insertId));
+    //sets users age preferences
+    await pConnection(queries.setUserPrefAges(req.body, result.insertId));
+    //sets user gender preferences
+    await pConnection(queries.setUserPrefGender(req.body, result.insertId));
+    //sets user height preferences
+    /// NOTE I HAVE CHANGED max_height type to SMALLINT
+    await pConnection(queries.setUserPrefHeight(req.body, result.insertId));
+    // sets user religion preferences
+    await pConnection(queries.setUserPrefReligion(req.body, result.insertId));
+    console.log(result.insertId);
     res.send({ status: 1 });
   } catch (error) {
     res.send({ status: 0, error: "Database refused to insert a new user" });
   }
 
   /* queries users
-      - multiple queries - add a whole user, not just email 
-
       - log in 
       - log out
 */
@@ -27,9 +47,14 @@ app.post("/selfie", (req, res) => {
   base64Data += base64Data.replace("+", " ");
   binaryData = Buffer.from(base64Data, "base64").toString("binary");
 
-  fs.writeFile(`./userImages/${req.body.user_id}.jpg`, binaryData, "binary", function (err) {
-    console.log(err); // writes out file without error, but it's not a valid image
-  });
+  fs.writeFile(
+    `./userImages/${req.body.user_id}.jpg`,
+    binaryData,
+    "binary",
+    function (err) {
+      console.log(err); // writes out file without error, but it's not a valid image
+    }
+  );
 });
 
 module.exports = app;
