b88820c474596067590b3d9edf0641c0cb6491a0
jessoy <81160034+jessoy@users.noreply.github.com>
2022-04-14 11:59:48 +0100

    user images updated

--- server.js
+++ server.js
@@ -6,12 +6,25 @@ const pConnection = require("./mysql/connection");
 const queries = require("./mysql/queriesUsers");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/userImages", express.static("userImages"));
+// go here if someone tries to access static file
 
 async function validateToken(req, res, next) {
-  console.log(req.headers)
+  console.log(req.headers);
   if (!req.headers.token) {
     res.send({ status: 0, error: "No token sent." });
     return;
